e3a11d150e0aea26e967527213658f05d32dd5e9
etiennelavigne <98097759+etiennelavigne@users.noreply.github.com>
2023-02-17 18:40:45 +0100

    commit

---
+++ ex03/ft_print_numbers.js
@@ -0,0 +1,6 @@
+function ft_print_numbers() {
+    let numbers = "0123456789";
+    console.log(numbers);
+}
+
+ft_print_numbers();
---
+++ ex04/ft_print_is_negative.js
@@ -0,0 +1,8 @@
+function showlines (n) {
+    if (n < 0) {
+      console.log("N");
+    } else if (n >= 0 ) {
+      console.log("P");
+    }
+  }
+showlines(2)
